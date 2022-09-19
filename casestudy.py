3b4c7f7a90835ca2496c2a73bbad62bd8689a028
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-09-19 20:18:50 +0500

    Update casestudy.py

--- casestudy.py
+++ casestudy.py
@@ -89,7 +89,7 @@ for x in range(100) :
         # Replace below: use max to make sure step can't go below 0
         step = max(0, step - 1)
     elif dice <= 5:
-        step = step + 1
+        step = step + 1 
     else:
         step = step + np.random.randint(1,7)
 

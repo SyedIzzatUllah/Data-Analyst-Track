4781e2ef15fc6aae5c27899d98fb7358144d1957
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-09-20 18:49:07 +0500

    Update casestudy.py

--- casestudy.py
+++ casestudy.py
@@ -12,7 +12,7 @@ print(np.random.rand())
 #Roll the dice 
 # Import numpy and set seed
 import numpy as np
-np.random.seed(123)
+np.random.seed(123) 
 
 # Use randint() to simulate a dice
 print(np.random.randint(1,7))
